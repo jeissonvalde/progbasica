9fb461a3f272e27db270fd39cbb042de4964c99f
jeissonv <jeissonvalderramam@gmail>
2016-10-08 21:54:53 -0500

    Nueva variable planeta

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ var gravedades = {
 	jupiter: 24.8
 };
 
-var mipeso;
+var mipeso, planeta;
 var peso_final;
 
 function inicio() {
 
 	mipeso = prompt("Escribe tu peso por favor.");
+	planeta = prompt("Escribe el planeta que quieres conocer tu peso (en minúsculas ej: marte o júpiter)");
 	peso_final = mipeso * gravedades.marte / gravedades.tierra;
 	peso_final = parseInt(peso_final);
 
