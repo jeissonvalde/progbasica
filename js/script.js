3759dba50c917957b7841ed9e5787b1b7dac8cce
jeissonv <jeissonvalderramam@gmail>
2016-10-08 21:56:58 -0500

    Solicita el planeta y condición de operación.

--- js/script.js
+++ js/script.js
@@ -10,8 +10,13 @@ var peso_final;
 function inicio() {
 
 	mipeso = prompt("Escribe tu peso por favor.");
-	planeta = prompt("Escribe el planeta que quieres conocer tu peso (en minúsculas ej: marte o júpiter)");
-	peso_final = mipeso * gravedades.marte / gravedades.tierra;
+	planeta = prompt("Escribe el planeta que quieres conocer tu peso \n (en minúsculas ej: marte o júpiter)");
+	if (planeta == "marte") {
+		peso_final = mipeso * gravedades.marte / gravedades.tierra;
+	}
+	if (planeta == "júpiter") {
+		peso_final = mipeso * gravedades.jupiter / gravedades.tierra;
+	}
 	peso_final = parseInt(peso_final);
 
 	document.write("Tu peso en marte es " + peso_final + " Kg");
